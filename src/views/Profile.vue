ef56169259debff3a478db0cb72082c47da42645
daryl <darylcoleoffice@gmail.com>
2019-07-03 16:07:45 +0900

    Add method to toggle form on profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,16 +3,62 @@
         User profile
         <div v-if="user">
             <img :src="imageUrl" />
-            {{ user }}
-            {{ imageHash }}
+            <div v-if="!editName" class="user-name">
+                {{ user.firstName }}
+                {{ user.lastName }}
+                <button @click="toggleEdit">edit name</button>
+            </div>
+            <div v-else>
+                <UserDetailsForm
+                    :edit-name="editName"
+                    :edit-email="false"
+                    :edit-password="false"
+                />
+            </div>
+            <div v-if="!editEmail" class="user-email">
+                {{ user.email }}
+                <button @click="toggleEdit">edit email</button>
+            </div>
+            <div v-else>
+                <UserDetailsForm
+                    :edit-name="false"
+                    :edit-email="editEmail"
+                    :edit-password="false"
+                />
+            </div>
+            <div v-if="!editPassword" class="user-password">
+                {{ user.password }}
+                <button @click="toggleEdit">edit password</button>
+            </div>
+            <div v-else>
+                <UserDetailsForm
+                    :edit-name="false"
+                    :edit-email="false"
+                    :edit-password="editPassword"
+                />
+            </div>
         </div>
     </section>
 </template>
 
 <script>
 import md5 from 'md5'
+import UserDetailsForm from '@/components/UserDetailsForm'
+
 export default {
     name: 'Profile',
+
+    components: {
+        UserDetailsForm,
+    },
+
+    data() {
+        return {
+            editName: false,
+            editEmail: false,
+            editPassword: false,
+        }
+    },
 
     computed: {
         user() {
@@ -23,6 +69,12 @@ export default {
                 return `https://www.gravatar.com/avatar/${md5(this.user.email)}`
             }
             return null
+        },
+    },
+
+    methods: {
+        toggleEdit() {
+            console.log('sdfdsf')
         },
     },
 }
